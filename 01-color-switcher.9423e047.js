3175bb6ae9b23f8e8a0a75fac716893da4147bfc
serhii95zhydkov <107838821+serhii95zhydkov@users.noreply.github.com>
2022-11-25 21:57:44 +0000

    Deploying to gh-pages from @ serhii95zhydkov/goit-js-hw-09@114a3eb267c537003ed245487b81dee6a4efd09d 🚀

---
+++ 01-color-switcher.9423e047.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(a=>{a.preventDefault(),t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(a=>{a.preventDefault(),t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.9423e047.js.map
