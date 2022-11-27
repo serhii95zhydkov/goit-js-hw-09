bdcf79be28e711021fa0572b6b70edf8f800a136
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-27 14:25:58 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+};
+
+refs.form.addEventListener('submit', onCallCreatePromise);
+
+function onCallCreatePromise(e) {
+  e.preventDefault();
+
+  const amount = refs.form.elements.amount.value;
+  const step = Number(refs.form.elements.step.value);
+  let delay = Number(refs.form.elements.delay.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  };
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  );
+};
